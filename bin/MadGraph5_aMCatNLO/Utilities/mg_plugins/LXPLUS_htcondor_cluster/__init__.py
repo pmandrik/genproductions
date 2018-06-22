53ee4555d79d258c77bfb54b94d145f8efe5225c
Petr <petr.mandrik@cern.ch>
2018-06-22 16:55:46 +0200

    initial

--- bin/MadGraph5_aMCatNLO/Utilities/mg_plugins/LXPLUS_htcondor_cluster/__init__.py
+++
@@ -1,9 +0,0 @@
-from LXPLUS_htcondor_cluster import LXPLUS_htcondor_cluster
-new_cluster = {'LXPLUS_htcondor_cluster': LXPLUS_htcondor_cluster}
-
-__author__ = 'Petr Mandrik'
-__email__ = 'petr.mandrik@ihep.ru'
-__version__ = (1,0,0)
-minimal_mg5amcnlo_version = (2,6,0) 
-maximal_mg5amcnlo_version = (1000,1000,1000)
-latest_validated_version = (2,6,0)
